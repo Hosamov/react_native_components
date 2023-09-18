d4b881c8623a582f0c47866962e26d1d822fa5a7
Hosamov <hosamov@hotmail.com>
2023-09-17 18:52:56 -0700

    update components, ListScreen, ImageScreen...

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,24 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+const HomeScreen = ({ navigation }) => { // Destructuring (props.navigation)
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')}
+        title="Go to Components Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title="Go to List Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate('Image')}
+        title="Go to Image Demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
