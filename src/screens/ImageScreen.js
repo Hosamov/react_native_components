f16a9a01a75b305cd62d2a942a479220261dfddc
Hosamov <hosamov@hotmail.com>
2023-09-18 07:43:43 -0700

    update ImageScreen component, add images

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-    return <Text>Image Screen</Text>;
-}
+  return (
+    <View>
+      <ImageDetail
+        title="Forest"
+        imageSource={require('../../assets/forest.jpg')}
+        score={1}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require('../../assets/beach.jpg')}
+        score={2}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require('../../assets/mountain.jpg')}
+        score={3}
+      />
+    </View>
+  );
+};
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({});
 
 export default ImageScreen;
